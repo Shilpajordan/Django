8cadd3cfb1ed5b9e18038215e283e0e05065ac4e
Shilpajordan <Shilpa.nair@dci-student.org>
2024-02-21 16:05:30 +0100

    templates

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat vegan!",
@@ -44,7 +45,7 @@ def my_page_challenge_by_number(request, month):
 def my_page(request, month): # adding the dynamic url parameter here
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}<h1>"
+        response_data = render_to_string("challenges/challenge.html")
         return HttpResponse(response_data)
     except :
         return HttpResponseNotFound("<h1>This month is not supported!<h1>")
