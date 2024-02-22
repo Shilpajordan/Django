c4d8fd7687059d82c36daa25a06d5b4d499d4a9a
Shilpajordan <Shilpa.nair@dci-student.org>
2024-02-22 15:40:23 +0100

    Tags, for tag,dynamic URLs,if tag, template inheritance

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -15,22 +15,25 @@ monthly_challenges = {
     "september": "Cook vegetarian food every day",
     "october": "Cook vegetarian food every day",
     "november": "Learn Django for at least 20 minutes every day",
-    "december": "Eat vegan!"
+    "december": None
 }
 
 # Create your views here.
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    return render(request,"challenges/index.html", {
+        "months":months
+    })
 
-    response_data = f"<ul>{list_items}</ul"
-    return HttpResponse(response_data)
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    # response_data = f"<ul>{list_items}</ul"
+    # return HttpResponse(response_data)
 
 def my_page_challenge_by_number(request, month):
     months = list(monthly_challenges.keys()) # returns a list of keys
